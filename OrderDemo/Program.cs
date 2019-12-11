744ea3f2175e52b271459367ecb37a01545306cb
panospd <anastapd@gmail.com>
2019-12-11 23:26:51 +0000

    Initial Commit

---
+++ OrderDemo/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+
+namespace OrderDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IReadOnlyCollection<Section> sections = UpdateOrder(2, 1);
+
+            IReadOnlyCollection<Section> sectionsToDisplay = OrderSections(sections);
+
+            foreach (var section in sectionsToDisplay)
+                Console.WriteLine(section.Name);
+        }
+
+        static IReadOnlyCollection<Section> OrderSections(IReadOnlyCollection<Section> sections)
+        {
+            List<Section> orderSections = new List<Section>();
+
+            Section firstSectionToAdd = sections.Single(x => x.PreviousId == null);
+
+            orderSections.Add(firstSectionToAdd);
+
+            List<Section> remainingSections = sections
+                .Where(x => x.PreviousId != null)
+                .ToList();
+
+            while (remainingSections.Any())
+            {
+                Section lastEntry = orderSections.Last();
+                Section nextSection = remainingSections.Single(x => x.PreviousId == lastEntry.Id);
+
+                orderSections.Add(nextSection);
+                remainingSections.Remove(nextSection);
+            }
+
+            return orderSections.ToArray();
+        }
+
+        static IReadOnlyCollection<Section> UpdateOrder(int sectionId, int? targetId)
+        {
+            IReadOnlyCollection<Section> sections = SectionsProvider.GetSections();
+
+            Section sectionToMove = sections.Single(x => x.Id == sectionId);
+            Section targetSection = sections.Single(x => x.Id == targetId);
+            Section nextFromTarget = sections.Single(x => x.PreviousId == sectionId);
+
+            Section newMovedSection = new Section(sectionToMove.Id, sectionToMove.Name, targetSection.PreviousId);
+            Section newTargetSection = new Section(targetSection.Id, targetSection.Name, sectionToMove.Id);
+            Section newNextFromTargetSection = new Section(nextFromTarget.Id, nextFromTarget.Name, sectionToMove.PreviousId);
+
+            var newSections = sections
+                .Where(x =>
+                    x.Id != sectionId &&
+                    x.Id != targetId &&
+                    x.PreviousId != sectionId)
+                .ToList();
+
+            newSections.AddRange(new List<Section>
+            {
+                newMovedSection,
+                newTargetSection,
+                newNextFromTargetSection
+            });
+
+            return newSections.ToArray();
+        }
+    }
+}
---
+++ OrderDemo/SectionsProvider.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+
+namespace OrderDemo
+{
+    public static class SectionsProvider
+    {
+        public static IReadOnlyCollection<Section> GetSections()
+        {
+            return new List<Section>
+            {
+                new Section(1, "Section 1", null),
+                new Section(2, "Section 2", 1),
+                new Section(3, "Section 3", 2),
+                new Section(4, "Section 4", 3),
+                new Section(5, "Section 5", 4)
+
+            };
+        }
+    }
+
+    public class Section
+    {
+        public Section(int id, string name, int? previousId)
+        {
+            Id = id;
+            Name = name;
+            PreviousId = previousId;
+        }
+
+        public int Id { get; }
+        public string Name { get; }
+        public int? PreviousId { get; }
+    }
+}
